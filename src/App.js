4d38e4fa16742415fc49e8a5ee19468d252b5c18
cassnova <daniel.rojascasanova@gmail.com>
2022-06-22 18:38:27 -0400

    Working in an about component, I push the container and elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import About from './components/AboutMe/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
     </div>
   );
 }
---
+++ src/components/AboutMe/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './About.css'
+
+function About() {
+    return (
+        <section className="sec-container">
+            <h2>About Me</h2>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat labore veritatis totam temporibus iure? Dignissimos.</p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
