47de31d8676d0703c3e8002e635ce645c9ada79c
cassnova <daniel.rojascasanova@gmail.com>
2022-06-23 18:59:35 -0400

    add card component

---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Card.css'
+
+function Card() {
+    return (
+        <div className="card-container">
+            <div className="img-container">
+                <img src="" alt="" />
+            </div>
+            <div
+                className="description-container">
+                <p></p>
+                <a href=""></a>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import './Projects.css'
+import Card from "../Cards/Card";
 
 function Projects() {
     return (
         <main className="container-main">
             <h2 className="title-elements">Projects</h2>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+
         </main>
     )
 }
