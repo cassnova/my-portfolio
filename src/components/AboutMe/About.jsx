b987f634d87e000488a433033d137039d170010d
cassnova <daniel.rojascasanova@gmail.com>
2022-06-23 13:55:42 -0400

    Worked in about me section with parallax effect

--- src/components/AboutMe/About.jsx
+++ src/components/AboutMe/About.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import './About.css'
 
+
 function About() {
+
+    const [offsetY, setOffsetY] = useState(0);
+    const handleScroll = () => setOffsetY(window.pageYOffset);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+
     return (
         <section className="sec-container">
-            <h2>About Me</h2>
+            <h2 style={{ transform: `translateY(${offsetY * 0.5}px)` }}>About Me</h2>
             <div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat labore veritatis totam temporibus iure? Dignissimos.</p>
             </div>
+
         </section>
     )
 }
