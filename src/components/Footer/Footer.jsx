e4a66a552679aec7bc3c741e16bf9761c2d7fb9e
cassnova <daniel.rojascasanova@gmail.com>
2022-07-09 21:52:43 -0400

    Working on footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import './Footer.css'
+import { AiFillInstagram, AiFillLinkedin, AiFillFacebook, AiFillGithub } from 'react-icons/ai';
+
+function Footer() {
+    return (
+        <footer className="footer-container">
+            <h1>Hola!</h1>
+            <div className="icons-container">
+
+                <a href="https://www.instagram.com/cassnova_/" target="_blank">
+                    <AiFillInstagram className="icon-container" />
+                </a>
+                <a href="https://www.linkedin.com/in/daniel-rojas-casanova/" target="_blank">
+                    <AiFillLinkedin className="icon-container" />
+                </a>
+                <a href="https://web.facebook.com/leinad.drc/" target="_blank">
+                    <AiFillFacebook className="icon-container" />
+                </a>
+                <a href="https://github.com/cassnova" target="_blank">
+                    <AiFillGithub className="icon-container" />
+                </a>
+
+            </div>
+        </footer>
+
+    )
+};
+
+export default Footer;
