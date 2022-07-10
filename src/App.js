3377f2ad9298ecdd3ac81fc5366795e80791c9dd
cassnova <daniel.rojascasanova@gmail.com>
2022-07-09 22:21:23 -0400

    Working on react bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import About from './components/AboutMe/About';
 import Projects from './components/Projects/Projects';
 import Footer from './components/Footer/Footer';
-import Data from './bd';
+import NavBarMenu from './components/nav/Nav';
+
+
 
 function App() {
   return (
     <div className="App">
+      <NavBarMenu />
       <Header />
       <About />
       <Projects />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,16 @@
-import { render } from "@testing-library/react";
 import React from "react";
 import './Header.css'
+
 
 
 function Header() {
     return (
         <header className="container-header">
+
             <h1>
                 <span className="border">Daniel</span>
                 <span className="border">Rojas</span>
                 <span className="border">Casanova</span>
-            </h1>
-            <h1>
-                <span className="wave">Daniel</span>
-                <span className="wave">Rojas</span>
-                <span className="wave">Casanova</span>
             </h1>
 
             <h2 className="fx">Front-End Developer</h2>
