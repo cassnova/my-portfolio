bf638dd12c86633d117e0fbd1258a5c165e7e1e2
cassnova <daniel.rojascasanova@gmail.com>
2023-02-03 12:41:56 -0300

    added new proyects

--- src/bd.js
+++ src/bd.js
@@ -1,46 +1,89 @@
 const Data = [
-    {
-        title: 'LIV STUDIO',
-        photo: './photolivstudio.jpg',
-        link: 'https://livstudio.cl',
-        description: 'Propuesta de diseño y desarrollo web ecommerce. En la tienda se implementa la API de Transbank Webpay para transacciones en Chile con tarjetas de débito, crédito y prepago. '
-    },
-    {
-        title: 'Podcasts',
-        photo: './photo1.jpg',
-        link: 'https://danielrojas-sprint1podcasts.netlify.app/',
-        description: 'Landing page de capítulos de podcasts sobre programación y la industria de la tecnología en general. '
-    },
-    {
-        title: 'Hotels',
-        photo: './photohotels.jpg',
-        link: 'https://reactivehotels.netlify.app/',
-        description: 'App web que facilita la búsqueda de hoteles, filtrando de acuerdo a las preferencias solicitadas en el buscador y los resultados se actualizan automáticamente.'
-    },
-    {
-        title: 'Gifos',
-        photo: './photogifos.jpg',
-        link: 'https://gifos-drc.netlify.app/',
-        description: 'En esta aplicación web el usuario puede buscar cualquier GIF con solo escribir la palabra clave en el buscador, entregando distintas sugerencias.'
-    },
-    {
-        title: 'Twitter',
-        photo: './photodevunited.jpg',
-        link: 'https://proyecto-prueba-5c922.web.app/',
-        description: 'App similar a Twitter. A través del ingreso por la cuenta personal de Google, el usuario puede comunicarse con personas de todas partes del mundo e interactuar en sus post.'
-    },
-    {
-        title: 'Blog Cafe',
-        photo: './photoblogcafe.jpg',
-        link: 'https://danielrojas-blogcafeproject.netlify.app/',
-        description: 'Diseño y desarrollo de blog del rubro cafetero, donde se entregan distintos tipos de consejos y cursos, permitiéndole obtener mayor visibilidad en la web. '
-    },
-    {
-        title: 'Calculator App',
-        photo: './photocalculator.jpg',
-        link: 'https://calculatorv1-drc.netlify.app/',
-        description: 'Diseño y desarrollo de aplicación web en donde el usuario podrá hacer cálculos matemáticos rápidamente. Esta calculadora fue programada con la última tecnología de desarrollo web.'
-    }
-]
+  {
+    title: "Gifos",
+    photo: "./photogifos.jpg",
+    link: "https://gifos-drc.netlify.app/",
+    description:
+      "En esta aplicación web el usuario puede buscar cualquier GIF con solo escribir la palabra clave en el buscador, entregando distintas sugerencias.",
+  },
+  {
+    title: "Twitter",
+    photo: "./photodevunited.jpg",
+    link: "https://proyecto-prueba-5c922.web.app/",
+    description:
+      "App similar a Twitter. A través del ingreso por la cuenta personal de Google, el usuario puede comunicarse con personas de todas partes del mundo e interactuar en sus post.",
+  },
+  {
+    title: "Hotels",
+    photo: "./photohotels.jpg",
+    link: "https://reactivehotels.netlify.app/",
+    description:
+      "App web que facilita la búsqueda de hoteles, filtrando de acuerdo a las preferencias solicitadas en el buscador y los resultados se actualizan automáticamente.",
+  },
+  {
+    title: "Calculator App",
+    photo: "./photocalculator.jpg",
+    link: "https://calculatorv1-drc.netlify.app/",
+    description:
+      "Diseño y desarrollo de aplicación web en donde el usuario podrá hacer cálculos matemáticos rápidamente. Esta calculadora fue programada con la última tecnología de desarrollo web.",
+  },
+  {
+    title: "Crypto App",
+    photo: "./crypto-app.jpg",
+    link: "https://drc-crypto-app.netlify.app/",
+    description:
+      "Diseño y desarrollo de aplicación web en donde el usuario podrá comparar el valor de una crypto moneda con una moneda local.",
+  },
+  {
+    title: "Weather App",
+    photo: "./weather-app.jpg",
+    link: "https://cassnova.github.io/weather-app/",
+    description:
+      "Diseño y desarrollo de aplicación web en donde el usuario podrá ver el clima, temperatura, humedad y velocidad del viento de cualquier ciudad que coloque en el input.",
+  },
+  {
+    title: "Crea Planeta",
+    photo: "./Creaplaneta-app.jpg",
+    link: "https://crea-planeta.netlify.app/",
+    description:
+      "Propuesta de diseño y desarrollo web para Crea Planeta. Esta web se desarrolló con tecnologías como React.js, React Router Dom, Email.js y Framer Motion",
+  },
+
+  {
+    title: "LIV STUDIO",
+    photo: "./photolivstudio.jpg",
+    link: "https://livstudio.cl",
+    description:
+      "Propuesta de diseño y desarrollo web ecommerce. En la tienda se implementa la API de Transbank Webpay para transacciones en Chile con tarjetas de débito, crédito y prepago. ",
+  },
+  {
+    title: "Exoespacial",
+    photo: "./exoespacial.jpg",
+    link: "https://exoespacial.com/",
+    description:
+      "Propuesta de diseño y desarrollo web para Relator Exoespacial. Blog donde pueden ver sus trabajos de relatos sobre experiencias, música en vivo, etc",
+  },
+  {
+    title: "Sabores como en Perú",
+    photo: "./sabores.jpg",
+    link: "https://saborescomoenperu.cl/",
+    description:
+      "Propuesta de diseño y desarrollo web para restaurant de comida peruana. Landing Page destacando imagen de marca donde los clientes pueden ver su menú.",
+  },
+  {
+    title: "Podcasts",
+    photo: "./photo1.jpg",
+    link: "https://danielrojas-sprint1podcasts.netlify.app/",
+    description:
+      "Landing page de capítulos de podcasts sobre programación y la industria de la tecnología en general. ",
+  },
+  {
+    title: "Blog Cafe",
+    photo: "./photoblogcafe.jpg",
+    link: "https://danielrojas-blogcafeproject.netlify.app/",
+    description:
+      "Diseño y desarrollo de blog del rubro cafetero, donde se entregan distintos tipos de consejos y cursos, permitiéndole obtener mayor visibilidad en la web. ",
+  },
+];
 
 export default Data;
